9bfe61af09aeff4c445a944171cf410005c6a5ba
JayKhot <jaykumar.khot@successive.tech>
2022-07-08 17:19:02 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.l0g('This is first node program')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const connectToMongo = require('./db');
+const config = require('dotenv').config()
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+connectToMongo();
+
+app.listen(PORT, () => {
+    console.log(`POC app listening on port ${PORT}`)
+})
